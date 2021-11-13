eb32daded008a79518379c576f3acfb983010400
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-13 00:55:41 -0700

    commit tree 72b929ace851193c2c17d96dfe8c4f0265d68f76

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+console.log(totalBatteries)
+
+// describe('reducer', function() {
+//     describe('batteries', function() {
+//       it('should have a `totalBatteries` variable', function() {
+//         expect(totalBatteries).to.exist;
+//       });
+  
+//       it('should have a number as a result', function() {
+//         expect(totalBatteries).to.be.a('number');
+//       });
+  
+//       it('should have made the sum of all the assembled batteries', function() {
+//         expect(totalBatteries).to.eql(31);
+//       });
+//     });
+//   });
+
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// let totalBatteries = (previousValue, currentValue) => previousValue + currentValue;
+
+// (batteryBatches.reduce(totalBatteries)); 
